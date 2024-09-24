1d9ae6e5334c4601205750eaaf2c5929aa91a409
mungyun <mungyun1234@naver.com>
2024-09-24 16:02:37 +0900

    [Feat] 요소 추가 기능 구현

--- components/Editor.js
+++ components/Editor.js
@@ -1,11 +1,50 @@
+import { useState, useRef } from "react";
 import styles from "./Editor.module.css";
-
+import { useAtom } from "jotai";
+import { datasAtom } from "@/store/atoms";
 function Editor() {
+  const [value, setValue] = useState("");
+  const [datas, setDatas] = useAtom(datasAtom);
+  const idRef = useRef(4);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) {
+      return;
+    }
+
+    setDatas((prevDatas) => [
+      {
+        id: idRef.current++,
+        content: value,
+        isDone: false,
+        date: new Date().getTime(),
+      },
+      ...prevDatas,
+    ]);
+    setValue("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit(e);
+    }
+  };
+
   return (
-    <div className={styles.Editor}>
-      <input />
-      <button>확인</button>
-    </div>
+    <form onSubmit={handleSubmit} className={styles.Editor}>
+      <input
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        type="text"
+      />
+      <button type="submit">확인</button>
+    </form>
   );
 }
 
