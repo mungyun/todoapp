588ac8fce75117c5a5774f049fdc6e992f9921f0
mungyun <mungyun1234@naver.com>
2024-09-24 22:41:27 +0900

    [Feat] Head 기능 추가

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,17 @@ import Editor from "@/components/Editor";
 import Header from "@/components/Header";
 import List from "@/components/List";
 import useLocalStorage from "@/hooks/useLocalStorage";
+import Head from "next/head";
 
 export default function Home() {
   const [datas, setDatas] = useLocalStorage("datas");
   return (
     <>
+      <Head>
+        <title>투두리스트</title>
+        <link rel="icon" href="/day.ico" />
+      </Head>
+
       <Header />
       <Editor setDatas={setDatas} />
       <List datas={datas} setDatas={setDatas} />
